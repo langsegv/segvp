e79b06ad76870e8f2e7039d52b91631fa9c810c3
lahit1 <naifcanbasci@gmail.com>
2026-04-30 01:55:45 +0300

    Holy format!! I'm about to die

--- src/tokenizer/read/symbol.cpp
+++ src/tokenizer/read/symbol.cpp
@@ -9,83 +9,82 @@
 #include <unordered_map>
 
 namespace segvc {
+    Token Tokenizer::readSymbol(std::istream &in) {
+        Token tok;
+        tok.startOffset = tok.endOffset = in.tellg();
+        tok.line = line;
+        tok.column = tok.startOffset - line_beg + 1;
 
-	Token Tokenizer::readSymbol(std::istream& in) {
-		Token tok;
-	        tok.startOffset = tok.endOffset = in.tellg();
-	        tok.line = line;
-	        tok.column = tok.startOffset - line_beg + 1;
+        char p = in.get();
+        tok.name = std::string(1, p);
 
-		char p = in.get();
-	        tok.name = std::string(1, p);
+        if (p == '+') {
+            p = in.peek();
+            switch (p) {
+                case '+':
+                case '=':
+                    tok.name += in.get();
+                    break;
+            }
+        } else if (p == '-') {
+            p = in.peek();
+            switch (p) {
+                case '-':
+                case '>':
+                case '=':
+                    tok.name += in.get();
+                    break;
+            }
+        } else
+            switch (p) {
+                case '/':
+                    p = in.peek();
+                    if (p == '=' || p == '/' || p == '*')
+                        tok.name += in.get();
+                    break;
+                case '*':
+                case '%':
+                case '^':
+                    p = in.peek();
+                    if (p == '=')
+                        tok.name += in.get();
+                    break;
+                case '&':
+                    p = in.peek();
+                    if (p == '&' || p == '=')
+                        tok.name += in.get();
+                    break;
+                case '|':
+                    p = in.peek();
+                    if (p == '|' || p == '=')
+                        tok.name += in.get();
+                    break;
+                case '.':
+                    p = in.peek();
+                    if (p == '.') {
+                        tok.name += in.get();
+                        if (p == '.')
+                            tok.name += in.get();
+                    }
+                    break;
+                case '<':
+                    p = in.peek();
+                    if (p == '<') {
+                        tok.name += in.get();
+                        if (p == '=')
+                            tok.name += in.get();
+                    } else if (p == '>') tok.name += in.get();
+                    break;
+                case '>':
+                    p = in.peek();
+                    if (p == '>') {
+                        tok.name += in.get();
+                        if (p == '=')
+                            tok.name += in.get();
+                    }
+                    break;
+            }
 
-		if(p == '+') {
-			p = in.peek();
-			switch(p) {
-				case '+':
-				case '=':
-				tok.name += in.get();
-				break;
-			}
-		} else if(p == '-') {
-	                p = in.peek();
-	                switch(p) {
-	                        case '-':
-				case '>':
-	                        case '=':
-	                        tok.name += in.get();
-	                        break;
-	                }
-	        } else switch(p) {
-			case '/':
-	        	        p = in.peek();
-				if(p == '=' || p == '/' || p == '*')
-	                        	tok.name += in.get();
-				break;
-			case '*':
-			case '%':
-			case '^':
-	        	        p = in.peek();
-				if(p == '=')
-	                        	tok.name += in.get();
-				break;
-			case '&':
-	                        p = in.peek();
-	                        if(p == '&' || p == '=')
-	                                tok.name += in.get();
-	                        break;
-	                case '|':
-	                        p = in.peek();
-	                        if(p == '|' || p == '=')
-	                                tok.name += in.get();
-	                        break;
-			case '.':
-	                        p = in.peek();
-	                        if(p == '.') {
-	                                tok.name += in.get();
-					if(p == '.')
-	                                	tok.name += in.get();
-				}
-	                        break;
-			case '<':
-	                        p = in.peek();
-	                        if(p == '<') {
-	                                tok.name += in.get();
-	                                if(p == '=')
-	                                        tok.name += in.get();
-	                        } else if(p == '>') tok.name += in.get();
-	                        break;
-			case '>':
-	                        p = in.peek();
-	                        if(p == '>') {
-	                                tok.name += in.get();
-	                                if(p == '=')
-	                                        tok.name += in.get();
-	                        }
-	                        break;
-	        }
-
-		return tok;
-	}
-
+        return tok;
+    }
 }
