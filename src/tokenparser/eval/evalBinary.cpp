142dcd7217ad05d6d068fa0e94a284419e781f54
lahit1 <naifcanbasci@gmail.com>
2026-05-14 19:41:05 +0300

    Documentation improvement

--- src/tokenparser/eval/evalBinary.cpp
+++ src/tokenparser/eval/evalBinary.cpp
@@ -2,6 +2,10 @@
 
 namespace segvc {
 
+
+	/** Evals binary expressions/operators from left to right recursively
+	 *  @returns An Expression pointer, if it's null, next tokens can't be evaluated as expression
+	 */
 	ExprPtr Tokenparser::evalBinaryLeftToRight(std::vector< std::pair<Tokens::Type, OPE>> &bindings, int index) {
 		ExprPtr main_expr = eval_order_exec(index + 1);
 
@@ -23,6 +27,9 @@ namespace segvc {
 		return main_expr;
 	}
 
+	/** Evals binary expressions/operators from right to left recursively
+	 *  @returns An Expression pointer, if it's null, next tokens can't be evaluated as expression
+	 */
 	ExprPtr Tokenparser::evalBinaryRightToLeft(std::vector< std::pair<Tokens::Type, OPE>> &bindings, int index) {
 		ExprPtr main_expr = eval_order_exec(index + 1);
 		
@@ -32,6 +39,7 @@ namespace segvc {
 					return std::make_shared<BinaryExpression>(
 						main_expr,
 						op,
+						// TODO: Evualation requires recursion, I don't think that's going to be a problem but I'm planing to change the method
 						evalBinaryRightToLeft(bindings, index));
 				}
 			}
